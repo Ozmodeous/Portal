bfa07a91426788f9617e28f933de50351507eb05
Ozmodeous <61337167+Ozmodeous@users.noreply.github.com>
2025-08-08 08:21:27 -0500

    Delete Portal.cpp

--- Portal.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (C) Developed by Pask, Published by Dark Tower Interactive SRL 2024. All Rights Reserved.
-
-#include "Portal.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Portal, "Portal" );
